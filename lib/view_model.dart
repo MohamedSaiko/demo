a71fede591a1691b3f4b0756c976fb0dc6be1507
MohamedSayed <Mohamedsaiko9@gmail.com>
2024-08-13 16:42:23 +0300

    update viewModel

--- lib/view_model.dart
+++ lib/view_model.dart
@@ -1,13 +1,21 @@
 import 'package:demo/models/album.dart';
 import 'package:demo/repositories/any_network_manager.dart';
+import 'package:demo/repositories/any_parser.dart';
+import 'package:flutter/foundation.dart';
 
 final class ViewModel {
-  final AnyNetworkManager _networkManager;
   late Future<List<Album>> albums;
+  final AnyNetworkManager _networkManager;
+  final AnyParser _parser;
 
-  ViewModel({required AnyNetworkManager networkManager}) : _networkManager = networkManager;
+  ViewModel(
+      {required AnyNetworkManager networkManager, required AnyParser parser})
+      : _networkManager = networkManager,
+        _parser = parser;
 
   void fetchAlbum() async {
-    albums = _networkManager.fetchAlbums('https://jsonplaceholder.typicode.com/albums');
+    albums = _networkManager.fetchAlbums(
+        url: 'https://jsonplaceholder.typicode.com/albums',
+        parseResponse: (response) => compute(_parser.parseAlbums, response));
   }
 }
